6ebb49baa0b086fea76138f1557d2a59427ce3fc
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-07-03 12:00:21 -0400

    Finish initial move

--- processor-service/processor-service.js
+++ processor-service/processor-service.js
@@ -1,10 +1,24 @@
-const path  = require('path');
-require('dotenv').config({path:  path.resolve(process.cwd(), '../.env')});
-
 const amqp = require('amqplib');
 
 // RabbitMQ connection string
-const messageQueueConnectionString = process.env.CLOUDAMQP_URL;
+const messageQueueConnectionString = `amqp://${process.env.RABBITMQ_URL ||
+  'localhost'}`;
+
+async function setupExchangesAndQueues(channel) {
+  console.log('Setting up RabbitMQ Exchanges/Queues');
+  // create exchange
+  await channel.assertExchange('processing', 'direct', { durable: true });
+
+  // create queues
+  await channel.assertQueue('processing.requests', { durable: true });
+  await channel.assertQueue('processing.results', { durable: true });
+
+  // bind queues
+  await channel.bindQueue('processing.requests', 'processing', 'request');
+  await channel.bindQueue('processing.results', 'processing', 'result');
+
+  console.log('Setup DONE');
+}
 
 async function listenForMessages() {
   // connect to Rabbit MQ
@@ -12,6 +26,7 @@ async function listenForMessages() {
 
   // create a channel and prefetch 1 message at a time
   let channel = await connection.createChannel();
+  await setupExchangesAndQueues(channel);
   await channel.prefetch(1);
 
   // create a second channel to send back the results
@@ -24,49 +39,55 @@ async function listenForMessages() {
 // utility function to publish messages to a channel
 function publishToChannel(channel, { routingKey, exchangeName, data }) {
   return new Promise((resolve, reject) => {
-    channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data), 'utf-8'), { persistent: true }, function (err, ok) {
-      if (err) {
-        return reject(err);
-      }
+    channel.publish(
+      exchangeName,
+      routingKey,
+      Buffer.from(JSON.stringify(data), 'utf-8'),
+      { persistent: true },
+      function(err, ok) {
+        if (err) {
+          return reject(err);
+        }
 
-      resolve();
-    })
+        resolve();
+      }
+    );
   });
 }
 
 // consume messages from RabbitMQ
 function consume({ connection, channel, resultsChannel }) {
   return new Promise((resolve, reject) => {
-    channel.consume("processing.requests", async function (msg) {
+    channel.consume('processing.requests', async function(msg) {
       // parse message
       let msgBody = msg.content.toString();
       let data = JSON.parse(msgBody);
       let requestId = data.requestId;
       let requestData = data.requestData;
-      console.log("Received a request message, requestId:", requestId);
+      console.log('Received a request message, requestId:', requestId);
 
       // process data
       let processingResults = await processMessage(requestData);
 
       // publish results to channel
       await publishToChannel(resultsChannel, {
-        exchangeName: "processing",
-        routingKey: "result",
+        exchangeName: 'processing',
+        routingKey: 'result',
         data: { requestId, processingResults }
       });
-      console.log("Published results for requestId:", requestId);
+      console.log('Published results for requestId:', requestId);
 
       // acknowledge message as processed successfully
       await channel.ack(msg);
     });
 
     // handle connection closed
-    connection.on("close", (err) => {
+    connection.on('close', err => {
       return reject(err);
     });
 
     // handle errors
-    connection.on("error", (err) => {
+    connection.on('error', err => {
       return reject(err);
     });
   });
@@ -76,7 +97,7 @@ function consume({ connection, channel, resultsChannel }) {
 function processMessage(requestData) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(requestData + "-processed")
+      resolve(requestData + '-processed');
     }, 5000);
   });
 }
--- web-service/web-service.js
+++ web-service/web-service.js
@@ -1,11 +1,8 @@
-const path  = require('path');
-require('dotenv').config({path:  path.resolve(process.cwd(), '../.env')});
-
 const express = require('express');
-const app = express();
 const http = require('http');
 const bodyParser = require('body-parser');
 const amqp = require('amqplib');
+const app = express();
 
 // Middleware
 app.use(bodyParser.json());
@@ -14,10 +11,11 @@ app.use(bodyParser.json());
 let lastRequestId = 1;
 
 // RabbitMQ connection string
-const messageQueueConnectionString = process.env.CLOUDAMQP_URL;
+const messageQueueConnectionString = `amqp://${process.env.RABBITMQ_URL ||
+  'localhost'}`;
 
 // handle the request
-app.post('/api/v1/processData', async function (req, res) {
+app.post('/api/v1/processData', async function(req, res) {
   // save request id and increment
   let requestId = lastRequestId;
   lastRequestId++;
@@ -28,26 +26,51 @@ app.post('/api/v1/processData', async function (req, res) {
 
   // publish the data to Rabbit MQ
   let requestData = req.body.data;
-  console.log("Published a request message, requestId:", requestId);
-  await publishToChannel(channel, { routingKey: "request", exchangeName: "processing", data: { requestId, requestData } });
+  console.log('Published a request message, requestId:', requestId);
+  await publishToChannel(channel, {
+    routingKey: 'request',
+    exchangeName: 'processing',
+    data: { requestId, requestData }
+  });
 
   // send the request id in the response
-  res.send({ requestId })
+  res.send({ requestId });
 });
 
 // utility function to publish messages to a channel
 function publishToChannel(channel, { routingKey, exchangeName, data }) {
   return new Promise((resolve, reject) => {
-    channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data), 'utf-8'), { persistent: true }, function (err, ok) {
-      if (err) {
-        return reject(err);
-      }
+    channel.publish(
+      exchangeName,
+      routingKey,
+      Buffer.from(JSON.stringify(data), 'utf-8'),
+      { persistent: true },
+      function(err, ok) {
+        if (err) {
+          return reject(err);
+        }
 
-      resolve();
-    })
+        resolve();
+      }
+    );
   });
 }
 
+async function setupExchangesAndQueues(channel) {
+  console.log('Setting up RabbitMQ Exchanges/Queues');
+  // create exchange
+  await channel.assertExchange('processing', 'direct', { durable: true });
+
+  // create queues
+  await channel.assertQueue('processing.requests', { durable: true });
+  await channel.assertQueue('processing.results', { durable: true });
+
+  // bind queues
+  await channel.bindQueue('processing.requests', 'processing', 'request');
+  await channel.bindQueue('processing.results', 'processing', 'result');
+
+  console.log('Setup DONE');
+}
 
 async function listenForResults() {
   // connect to Rabbit MQ
@@ -55,35 +78,40 @@ async function listenForResults() {
 
   // create a channel and prefetch 1 message at a time
   let channel = await connection.createChannel();
+  await setupExchangesAndQueues(channel);
   await channel.prefetch(1);
 
   // start consuming messages
   await consume({ connection, channel });
 }
 
-
 // consume messages from RabbitMQ
 function consume({ connection, channel, resultsChannel }) {
   return new Promise((resolve, reject) => {
-    channel.consume("processing.results", async function (msg) {
+    channel.consume('processing.results', async function(msg) {
       // parse message
       let msgBody = msg.content.toString();
       let data = JSON.parse(msgBody);
       let requestId = data.requestId;
       let processingResults = data.processingResults;
-      console.log("Received a result message, requestId:", requestId, "processingResults:", processingResults);
+      console.log(
+        'Received a result message, requestId:',
+        requestId,
+        'processingResults:',
+        processingResults
+      );
 
       // acknowledge message as received
       await channel.ack(msg);
     });
 
     // handle connection closed
-    connection.on("close", (err) => {
+    connection.on('close', err => {
       return reject(err);
     });
 
     // handle errors
-    connection.on("error", (err) => {
+    connection.on('error', err => {
       return reject(err);
     });
   });
@@ -92,11 +120,11 @@ function consume({ connection, channel, resultsChannel }) {
 // Start the server
 const PORT = 3000;
 server = http.createServer(app);
-server.listen(PORT, "localhost", function (err) {
+server.listen(PORT, function(err) {
   if (err) {
     console.error(err);
   } else {
-    console.info("Listening on port %s.", PORT);
+    console.info('Listening on port %s.', PORT);
   }
 });
 
